7d422e0bd7120f1c842ac5532902df544e86bdb4
Matous Kucera <matous.kucera@envelon.cz>
2013-06-18 11:55:17 +0200

    Initial grails plugin setup

---
+++ CountrySelectorGrailsPlugin.groovy
@@ -0,0 +1,19 @@
+class CountrySelectorGrailsPlugin {
+   def version = "0.1"
+   def grailsVersion = "2.0 > *"
+
+   def title = "Country Selector Plugin"
+   def author = "Matouš Kučera"
+   def authorEmail = "matous.kucera@tado.com"
+   def description = "Provides an advanced country selector in your grails application."
+
+   def documentation = "http://deigote.github.io/grails-timestamped/"
+
+   def license = "GPL3"
+
+   def organization = [name: "tado GmbH", url: "http://tado.com/"]
+
+   def issueManagement = [system: "Github", url: "https://github.com/EnergyBob/grails-country-selector/issues"]
+
+   def scm = [url: "https://github.com/EnergyBob/grails-country-selector"]
+}
---
+++ grails-app/conf/BuildConfig.groovy
@@ -0,0 +1,16 @@
+grails.project.work.dir = 'target'
+
+grails.project.dependency.resolution = {
+   inherits 'global'
+   log 'warn'
+
+   repositories {
+      grailsCentral()
+   }
+
+   plugins {
+      build ':release:2.2.1', ':rest-client-builder:1.0.3', {
+         export = false
+      }
+   }
+}
