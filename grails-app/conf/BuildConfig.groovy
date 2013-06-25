93f37cf53f0352243f25849822ec5089fccb9042
Matous Kucera <matous.kucera@envelon.cz>
2013-06-25 16:59:59 +0200

    Fix plugin dependencies

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -6,14 +6,13 @@ grails.project.dependency.resolution = {
 
    repositories {
       grailsCentral()
-      grailsRepo "http://grails.org/plugins"
    }
 
    plugins {
       build ':release:2.2.1' { export = false }
       build ':rest-client-builder:1.0.3' { export = false }
-      runtime ':resources:1.1.6' { export = false }
-      runtime ":jquery:1.8.3" { export = false }
-      runtime ":jquery-ui:1.8.24" { export = false }
+      runtime ':resources:1.1.6'
+      runtime ":jquery:1.8.3"
+      runtime ":jquery-ui:1.8.24"
    }
 }
