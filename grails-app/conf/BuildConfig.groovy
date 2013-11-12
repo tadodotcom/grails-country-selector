9156924289cf702e44ea41e36493064151d39b5b
Matous Kucera <matous.kucera@envelon.cz>
2013-11-12 16:29:19 +0100

    Increase grails version, do not exclude resource plugin as it is required

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -15,9 +15,7 @@ grails.project.dependency.resolution = {
       build(':release:2.2.1', ':rest-client-builder:1.0.3') {
          export = false
       }
-      compile ":resources:1.1.6" {
-         export = false
-      }
+      compile ":resources:1.2.1"
       compile ":jquery:1.8.3"
       compile ":jquery-ui:1.8.24"
    }
