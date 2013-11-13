da41d407d8cae8195452af501952261c3ecae507
Matous Kucera <matous.kucera@envelon.cz>
2013-11-13 09:21:43 +0100

    version 1.0

--- CountrySelectorGrailsPlugin.groovy
+++ CountrySelectorGrailsPlugin.groovy
@@ -1,23 +1,23 @@
 import org.grails.plugins.countrySelector.SearchResourceBundleMessageSource
 
 class CountrySelectorGrailsPlugin {
-   def version = "0.2.6"
+   def version = "1.0"
    def grailsVersion = "2.0 > *"
 
    def title = "Country Selector Plugin"
    def author = "Matouš Kučera"
    def authorEmail = "matous.kucera@tado.com"
-   def description = "An advanced country selector in your grails application."
+   def description = "Advanced country selector for forms."
 
-   def documentation = "http://energybob.github.io/grails-country-selector"
+   def documentation = "http://tadodotcom.github.io/grails-country-selector"
 
    def license = "GPL3"
 
    def organization = [name: "tado GmbH", url: "http://tado.com/"]
 
-   def issueManagement = [system: "Github", url: "https://github.com/EnergyBob/grails-country-selector/issues"]
+   def issueManagement = [system: "Github", url: "https://github.com/tadodotcom/grails-country-selector/issues"]
 
-   def scm = [url: "https://github.com/EnergyBob/grails-country-selector"]
+   def scm = [url: "https://github.com/tadodotcom/grails-country-selector"]
 
    def loadAfter = ['i18n']
 }
